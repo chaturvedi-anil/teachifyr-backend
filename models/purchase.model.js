7187cd56bb5356214f744f03d1ddb82d8334d293
Anil Chaturvedi <chaturvedia560@gmail.com>
2024-10-05 17:24:10 +0530

    fixed purchased issue

--- models/purchase.model.js
+++ models/purchase.model.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const purchaseSchema = mongoose.Schema({
     courseId: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: "Course"
+        ref: "Courses"
     },
     userId: {
         type: mongoose.Schema.Types.ObjectId,
